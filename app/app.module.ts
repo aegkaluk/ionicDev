cb60ea1d2f7654f5862dba3fada5d5e2ef3b4d8d
aegkaluk <aegkaluk@gmail.com>
2018-06-23 11:54:26 +0700

    commit network

---
+++ app/app.module.ts
@@ -0,0 +1,57 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+import { Camera } from '@ionic-native/camera';
+import { File } from '@ionic-native/file';
+import { FileTransfer } from '@ionic-native/file-transfer';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
+import { HttpModule } from '@angular/http';
+
+import { MyproviderProvider } from '../providers/myprovider/myprovider';
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { CamPage } from '../pages/cam/cam';
+import { FileDevPage } from '../pages/file-dev/file-dev';
+import { ScanPage } from '../pages/scan/scan';
+import { HttpPage } from '../pages/http/http';
+import { NetworkPage } from '../pages/network/network';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    CamPage,
+    FileDevPage,
+    ScanPage,
+    HttpPage,
+    NetworkPage,
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    CamPage,
+    FileDevPage,
+    ScanPage,
+    HttpPage,
+    NetworkPage,
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    Camera,
+    File,
+    FileTransfer,
+    BarcodeScanner,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    MyproviderProvider
+  ]
+})
+export class AppModule {}
